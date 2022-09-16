6fce626902e02b8abbf88c93463caed6f9988a91
aliakseikln <aliakseikln@gmail.com>
2022-09-16 23:42:55 +0300

    Added DI and VM

---
+++ app/src/main/java/com/example/weather2/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.example.weather2.di
+
+import com.example.weather2.data.repopsitory.WeatherRepositoryImpl
+import com.example.weather2.domain.repository.WeatherRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(weatherRepositoryImpl: WeatherRepositoryImpl): WeatherRepository
+}
+
